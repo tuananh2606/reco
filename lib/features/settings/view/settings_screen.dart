6d1087ff2ecf6963a31981d84f8495f47dd64599
tuananh2606 <ngdtuananh01@gmail.com>
2024-07-23 17:26:28 +0700

    feature: add setting screen

--- lib/features/settings/view/settings_screen.dart
+++ lib/features/settings/view/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:reco/utils/constants/sizes.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -9,6 +11,47 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.md),
+                child: Text(
+                  'Edit Profile',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              ColoredBox(
+                color: Colors.grey.shade800,
+                child: ListTile(
+                  title: SizedBox(
+                    height: 40,
+                    child: Text(
+                      'Username',
+                      textAlign: TextAlign.start,
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                  ),
+                  trailing: const Icon(
+                    LineIcons.angleRight,
+                    size: 18,
+                  ),
+                  leading: const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1721419336937-86dc7d479f2c?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
